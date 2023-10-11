64087424b722c9e29f9a7429db53866bc70af3c3
BWOH <bwoh@outlook.kr>
2023-10-11 11:05:08 +0900

    🚀 ViewModel 추가

--- app/src/main/java/kr/ac/kumoh/ce/prof01/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/s23w04carddealer/MainActivity.kt
@@ -2,32 +2,40 @@ package kr.ac.kumoh.ce.prof01.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.prof01.s23w04carddealer.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
+    private lateinit var model: CardDealerViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(R.layout.activity_main)
         setContentView(main.root)
 
-        //binding.card1.setImageResource(R.drawable.c_2_of_hearts)
-        //val c = Random.nextInt(52)
-        // TODO: 랜덤으로 변경할 것
-        val c = 37
-        Log.i("Test", "$c : ${getCardName(c)}")
+        model = ViewModelProvider(this)[CardDealerViewModel::class.java]
+        model.cards.observe(this, Observer {
+            val res = IntArray(5)
+            for (i in it.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+            }
 
-        val res = resources.getIdentifier(
-            getCardName(c),
-            "drawable",
-            packageName
-        )
+            main.card1.setImageResource(res[0])
+//            main.card2.setImageResource(res[1])
+//            main.card3.setImageResource(res[2])
+//            main.card4.setImageResource(res[3])
+//            main.card5.setImageResource(res[4])
+        })
 
-        main.card1.setImageResource(res)
+        main.btnShuffle.setOnClickListener {
+            model.shuffle()
+        }
     }
 
     private fun getCardName(c: Int) : String {
